7b13b68fbbc9b4d2303dc22a40871d3ab3c1e995
Alejandro Mongua <alejandro.mongua@gmail.com>
2020-08-29 20:46:41 -0500

    Added snapshot test

--- __mocks__/firebase/app.js
+++ __mocks__/firebase/app.js
@@ -1,10 +1,36 @@
+const tareas = [
+  {
+    text: 'Tarea 1',
+    completed: false,
+  },
+  {
+    text: 'Tarea 2',
+    completed: true,
+  },
+  {
+    text: 'Tarea 3',
+    completed: false,
+  },
+  {
+    text: 'Tarea 4',
+    completed: true,
+  },
+  {
+    text: 'Tarea 5',
+    completed: false,
+  },
+]
+
 module.exports = {
   initializeApp: () => null,
   analytics: () => null,
   firestore: () => ({
     collection: () => ({
       get: () => ({
-        docs: []
+        docs: tareas.map((tarea, index) => ({
+          data: () => tarea,
+          id: String(index),
+        }))
       })
     })
   }),
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,7 +2,15 @@ import '@babel/polyfill'
 import React from 'react';
 import App from '../components/App'
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer'
 
 it("renders without crashing", () => {
-  render(<App />);
+  render(<App />)
 });
+
+it('matches snapshot', () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot()
+})
